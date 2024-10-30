e6a7c227b0049612fca8f8c2aeef54cb46727daf
michael12345678 <michaelferreiradesousa@gmail.com>
2024-10-30 13:53:35 -0300

    inclusão de mensagem

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function UploadPage() {
   const [file, setFile] = useState(null);
@@ -9,43 +9,53 @@ function UploadPage() {
 
   const handleUpload = async () => {
     if (!file) {
-      console.error('Nenhum arquivo selecionado');
+      console.error("Nenhum arquivo selecionado");
       return;
     }
 
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
 
     try {
-      const response = await fetch('https://backend-termos-crutac.onrender.com/upload', {
-        method: 'POST',
-        body: formData,
-      });
+      const response = await fetch(
+        "https://backend-termos-crutac.onrender.com/upload",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
       if (response.ok) {
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
-        
-        // Cria um link temporário para baixar o ZIP
-        const link = document.createElement('a');
+
+        const link = document.createElement("a");
         link.href = url;
-        link.setAttribute('download', 'termos_estagio.zip');
+        link.setAttribute("download", "termos_estagio.zip");
         document.body.appendChild(link);
         link.click();
         link.parentNode.removeChild(link);
-        
       } else {
-        console.error('Erro ao gerar PDFs');
+        console.error("Erro ao gerar PDFs");
       }
     } catch (error) {
-      console.error('Erro na requisição:', error);
+      console.error("Erro na requisição:", error);
     }
   };
 
   return (
     <div>
+      <div>
+        <p>
+          Amor, lembrando que a planilha tem que ter as colunas com os seguintes
+          títulos: Nome; CPF; Matricula; Endereco; Cidade/UF; Telefone
+        </p>
+        <h6>E lembra também que Te amo</h6>
+      </div>
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} disabled={!file}>Gerar e Baixar Termos</button>
+      <button onClick={handleUpload} disabled={!file}>
+        Gerar e Baixar Termos
+      </button>
     </div>
   );
 }
